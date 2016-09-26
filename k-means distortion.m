a6a576f0f5c4e5af8a7841bfc1dabb71e218d17b
verowulf <vati27@gmail.com>
2016-09-26 18:37:37 +0900

    Update k-means distortion.m
    Added more efficient version.

--- k-means distortion.m
+++ k-means distortion.m
@@ -3,9 +3,15 @@
 % Insert into runkMeans.m in ex7 folder
 % NOTE: Must change 'i' of existing 'for loop' into something like 'iter'
 
-SumD = 0;
+Centroids = zeros(m,n);
 for i = 1:m
-    SumD = SumD + sum( ( X(i,:) - centroids(idx(i), :) ) .^ 2 );
+    Centroids(i,:) = centroids(idx(i), :);
 end
+Distortion = sum( sum((X - Centroids).^2) ) / m;
 
-Distortion = SumD / m;
+% More intuitive, but slower version
+% SumD = 0;
+% for i = 1:m
+%    SumD = SumD + sum( ( X(i,:) - centroids(idx(i), :) ) .^ 2 );
+% end
+% Distortion = SumD / m;
